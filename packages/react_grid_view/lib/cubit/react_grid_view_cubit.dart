45104a9e3d65e8951d8cc3ecd108a2e9e51606ff
Hyeonki Hong <hhk7734@gmail.com>
2021-01-10 19:13:44 +0900

    react_grid_view: view: implement ReactGridView

--- packages/react_grid_view/lib/cubit/react_grid_view_cubit.dart
+++ packages/react_grid_view/lib/cubit/react_grid_view_cubit.dart
@@ -1,8 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../react_grid_view.dart';
+
 part 'react_grid_view_state.dart';
 
 class ReactGridViewCubit extends Cubit<ReactGridViewState> {
   ReactGridViewCubit() : super(ReactGridViewInitial());
+
+  List<ReactPositioned> _children;
+  set children(List<ReactPositioned> children) => _children = children;
+
+  ReactGridViewModel _reactGridViewModel;
+  set reactGridViewModel(ReactGridViewModel model) =>
+      _reactGridViewModel = model;
+
+  set width(double width) =>
+      _reactGridViewModel = _reactGridViewModel.copyWith(width: width);
+
+  void initView() {
+    emit(ReactGridViewUpdateState(_children, _reactGridViewModel));
+  }
 }
--- packages/react_grid_view/lib/view/react_grid_view.dart
+++ packages/react_grid_view/lib/view/react_grid_view.dart
@@ -1,8 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../react_grid_view.dart';
 
 class ReactGridView extends StatelessWidget {
+  ReactGridView({
+    Key key,
+    List<ReactPositioned> children,
+    @required int crossAxisCount,
+    double crossAxisSpacing = 10,
+    double gridAspectRatio = 3 / 4,
+    @required int mainAxisCount,
+    double mainAxisSpacing = 10,
+  }) : super(key: key) {
+    _cubit.children = children ?? <ReactPositioned>[];
+    _cubit.reactGridViewModel = ReactGridViewModel(
+      crossAxisCount: crossAxisCount,
+      crossAxisSpacing: crossAxisSpacing,
+      gridAspectRatio: gridAspectRatio,
+      mainAxisCount: mainAxisCount,
+      mainAxisSpacing: mainAxisSpacing,
+    );
+  }
+
+  final ReactGridViewCubit _cubit = ReactGridViewCubit();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        _cubit.width = constraints.maxWidth;
+        return BlocProvider(
+          create: (_) => _cubit,
+          child: SingleChildScrollView(
+            child: BlocBuilder<ReactGridViewCubit, ReactGridViewState>(
+              cubit: _cubit..initView(),
+              buildWhen: (previous, current) {
+                if (current is ReactGridViewUpdateState) return true;
+                return false;
+              },
+              builder: (context, state) {
+                if (state is ReactGridViewUpdateState) {
+                  double height = constraints.maxHeight > state.model.height
+                      ? constraints.maxHeight
+                      : state.model.height;
+                  return GestureDetector(
+                    child: Container(
+                      height: height,
+                      child: Stack(children: state.children),
+                    ),
+                  );
+                }
+                return Container(
+                  child: const Center(child: CircularProgressIndicator()),
+                  height: constraints.maxHeight,
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
   }
 }
