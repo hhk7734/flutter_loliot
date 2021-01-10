da48e9dc868da4bb7aab2bcb2db7b3c630261d3d
Hyeonki Hong <hhk7734@gmail.com>
2021-01-10 14:47:28 +0900

    react_grid_view: cubit: add ReactGridViewUpdateState

--- packages/react_grid_view/lib/cubit/react_grid_view_state.dart
+++ packages/react_grid_view/lib/cubit/react_grid_view_state.dart
@@ -2,9 +2,19 @@ part of 'react_grid_view_cubit.dart';
 
 abstract class ReactGridViewState extends Equatable {
   const ReactGridViewState();
+}
 
+class ReactGridViewInitial extends ReactGridViewState {
   @override
   List<Object> get props => [];
 }
 
-class ReactGridViewInitial extends ReactGridViewState {}
+class ReactGridViewUpdateState extends ReactGridViewState {
+  const ReactGridViewUpdateState(this.children, this.model);
+
+  final List<ReactPositioned> children;
+  final ReactGridViewModel model;
+
+  @override
+  List<Object> get props => [children.length, model];
+}
