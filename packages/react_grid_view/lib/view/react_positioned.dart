c607f06d0e82bd4acf9275503644be5a1def8cbc
Hyeonki Hong <hhk7734@gmail.com>
2021-01-12 17:13:17 +0900

    react_grid_view: view: implement resizeOnPan* to resize

--- packages/react_grid_view/lib/view/react_positioned.dart
+++ packages/react_grid_view/lib/view/react_positioned.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../react_grid_view.dart';
+
+part 'resizable_overlay.dart';
+part 'resize_gesture_detector.dart';
 
 class ReactPositioned {
   ReactPositioned({
@@ -45,7 +49,6 @@ class ReactPositioned {
         key: key,
         child: child,
         index: index,
-        model: model,
       );
 }
 
@@ -54,13 +57,11 @@ class _ReactPositioned extends StatefulWidget {
     Key key,
     this.child,
     this.index,
-    this.model,
   }) : super(key: key);
+
   final Widget child;
 
   final int index;
-
-  final ReactPositionedModel model;
 
   @override
   _ReactPositionedState createState() => _ReactPositionedState();
@@ -75,11 +76,18 @@ class _ReactPositionedState extends State<_ReactPositioned> {
 
   Offset offset;
 
-  int previousOffsetX = 0;
-  int previousOffsetY = 0;
+  _ResizableOverlay overlay;
 
-  int startOffsetX = 0;
-  int startOffsetY = 0;
+  int previousCrossAxisCount;
+  int previousCrossAxisOffsetCount;
+  int previousMainAxisCount;
+  int previousMainAxisOffsetCount;
+
+  double startHeight;
+  double startLeft;
+  ReactPositionedModel startModel;
+  double startTop;
+  double startWidth;
 
   @override
   void initState() {
@@ -133,6 +141,7 @@ class _ReactPositionedState extends State<_ReactPositioned> {
                         width: widthWithoutMargin,
                       ),
                     ),
+                    onDragEnd: onDragEndCallback,
                     onDragStarted: onDragStartedCallback,
                     onDragUpdate: onDragUpdateCallback,
                   )
@@ -162,7 +171,6 @@ class _ReactPositionedState extends State<_ReactPositioned> {
   double get height =>
       model.mainAxisCount * reactGridViewModel.mainAxisStride +
       mainAxisOverflow;
-
   double get heightWithoutMargin => height - margin.vertical;
 
   double get left =>
@@ -181,6 +189,22 @@ class _ReactPositionedState extends State<_ReactPositioned> {
     return EdgeInsets.fromLTRB(cross, main, cross, main);
   }
 
+  double get maxHeight =>
+      model.maxMainAxisCount * reactGridViewModel.mainAxisStride +
+      mainAxisOverflow;
+  double get maxWidth =>
+      model.maxCrossAxisCount * reactGridViewModel.crossAxisStride +
+      crossAxisOverflow;
+
+  double get minHeight =>
+      model.minMainAxisCount * reactGridViewModel.mainAxisStride +
+      mainAxisOverflow;
+  double get minWidth =>
+      model.minCrossAxisCount * reactGridViewModel.crossAxisStride +
+      crossAxisOverflow;
+
+  bool get resizable => (model.horizontalResizable || model.verticalResizable);
+
   double get top =>
       model.mainAxisOffsetCount * reactGridViewModel.mainAxisStride -
       mainAxisOverflow / 2;
@@ -188,46 +212,227 @@ class _ReactPositionedState extends State<_ReactPositioned> {
   double get width =>
       model.crossAxisCount * reactGridViewModel.crossAxisStride +
       crossAxisOverflow;
-
   double get widthWithoutMargin => width - margin.horizontal;
 
   // move
 
   void onDragStartedCallback() {
     offset = Offset.zero;
-    startOffsetX = model.crossAxisOffsetCount;
-    startOffsetY = model.mainAxisOffsetCount;
-    previousOffsetX = 0;
-    previousOffsetY = 0;
+    startModel = model.copyWith();
+
+    previousCrossAxisOffsetCount = startModel.crossAxisOffsetCount;
+    previousMainAxisOffsetCount = startModel.mainAxisOffsetCount;
   }
 
   void onDragUpdateCallback(DragUpdateDetails details) {
     offset += details.delta;
-    int offsetX = (offset.dx / reactGridViewModel.crossAxisStride).round();
-    int offsetY = (offset.dy / reactGridViewModel.mainAxisStride).round();
+    int crossAxisOffsetCount =
+        (offset.dx / reactGridViewModel.crossAxisStride).round() +
+            startModel.crossAxisOffsetCount;
+    int mainAxisOffsetCount =
+        (offset.dy / reactGridViewModel.mainAxisStride).round() +
+            startModel.mainAxisOffsetCount;
 
-    if (offsetX == previousOffsetX && offsetY == previousOffsetY) return;
+    if (crossAxisOffsetCount < 0 || mainAxisOffsetCount < 0) return;
 
-    previousOffsetX = offsetX;
-    previousOffsetY = offsetY;
+    ReactPositionedModel nextModel = model.copyWith(
+        crossAxisOffsetCount: crossAxisOffsetCount,
+        mainAxisOffsetCount: mainAxisOffsetCount);
 
-    offsetX += startOffsetX;
-    offsetY += startOffsetY;
+    if (reactGridViewModel.checkOverflow(nextModel)) return;
 
-    if (model.crossAxisOffsetCount == offsetX &&
-        model.mainAxisOffsetCount == offsetY) return;
+    if (previousCrossAxisOffsetCount != crossAxisOffsetCount ||
+        previousMainAxisOffsetCount != mainAxisOffsetCount) {
+      previousCrossAxisOffsetCount = crossAxisOffsetCount;
+      previousMainAxisOffsetCount = mainAxisOffsetCount;
+      _cubit.movedChild(
+          widget.index,
+          model.copyWith(
+              crossAxisOffsetCount: crossAxisOffsetCount,
+              mainAxisOffsetCount: mainAxisOffsetCount));
+    }
+  }
 
-    if (offsetX < 0 ||
-        offsetX + model.crossAxisCount > reactGridViewModel.crossAxisCount)
-      return;
+  // resize
 
-    if (offsetY < 0 ||
-        offsetY + model.mainAxisCount > reactGridViewModel.mainAxisCount)
-      return;
+  void onDragEndCallback(DraggableDetails details) {
+    if (resizable && overlay == null) {
+      final RenderBox box = context.findRenderObject() as RenderBox;
+      final Offset globalOffset = box.localToGlobal(Offset.zero);
+      overlay = _ResizableOverlay(
+        clickableWidth: reactGridViewModel.clickableWidth,
+        horizontalResizable: model.horizontalResizable,
+        height: height,
+        width: width,
+        margin: margin,
+        left: globalOffset.dx,
+        top: globalOffset.dy,
+        onPanDown: resizeOnPanDownCallback,
+        onPanDownCenter: resizeOnPanDownCenterCallback,
+        onPanEnd: resizeOnPanEndCallback,
+        onPanUpdateBottom: resizeOnPanUpdateBottomCallback,
+        onPanUpdateLeft: resizeOnPanUpdateLeftCallback,
+        onPanUpdateRight: resizeOnPanUpdateRightCallback,
+        onPanUpdateTop: resizeOnPanUpdateTopCallback,
+        overlayState: Overlay.of(context, debugRequiredFor: widget),
+        verticalResizable: model.verticalResizable,
+      );
+    }
+  }
 
-    _cubit.movedChild(
-        widget.index,
-        model.copyWith(
-            crossAxisOffsetCount: offsetX, mainAxisOffsetCount: offsetY));
+  void resizeOnPanDownCenterCallback(DragDownDetails details) {
+    if (resizable && overlay != null) {
+      overlay.close();
+      overlay = null;
+    }
+  }
+
+  void resizeOnPanDownCallback(DragDownDetails details) {
+    offset = Offset.zero;
+    startHeight = overlay.height;
+    startLeft = overlay.left;
+    startModel = model.copyWith();
+    startTop = overlay.top;
+    startWidth = overlay.width;
+
+    previousCrossAxisCount = startModel.crossAxisCount;
+    previousMainAxisCount = startModel.mainAxisCount;
+  }
+
+  void resizeOnPanEndCallback(DragEndDetails details) {
+    final RenderBox box = context.findRenderObject() as RenderBox;
+    final Offset globalOffset = box.localToGlobal(Offset.zero);
+    overlay.height = height;
+    overlay.left = globalOffset.dx;
+    overlay.top = globalOffset.dy;
+    overlay.width = width;
+    overlay.overlayEntry.markNeedsBuild();
+  }
+
+  void resizeOnPanUpdateBottomCallback(DragUpdateDetails details) {
+    offset += details.delta;
+    overlay.height = startHeight + offset.dy;
+
+    int maxMainAxisCount =
+        reactGridViewModel.mainAxisCount - startModel.mainAxisOffsetCount;
+    if (maxMainAxisCount >= startModel.maxMainAxisCount)
+      overlay.height = overlay.height.clamp(minHeight, maxHeight + 10);
+    else {
+      overlay.height = overlay.height.clamp(
+          minHeight,
+          maxMainAxisCount * reactGridViewModel.mainAxisStride +
+              mainAxisOverflow +
+              10);
+    }
+
+    overlay.overlayEntry.markNeedsBuild();
+
+    int mainAxisCount =
+        (overlay.height / reactGridViewModel.mainAxisStride).floor();
+
+    if (previousMainAxisCount != mainAxisCount) {
+      previousMainAxisCount = mainAxisCount;
+      _cubit.resizedChild(
+          widget.index, startModel.copyWith(mainAxisCount: mainAxisCount));
+    }
+  }
+
+  void resizeOnPanUpdateLeftCallback(DragUpdateDetails details) {
+    offset += details.delta;
+    overlay.width = startWidth - offset.dx;
+
+    int maxCrossAxisCount =
+        startModel.crossAxisOffsetCount + startModel.crossAxisCount;
+    if (maxCrossAxisCount >= startModel.maxCrossAxisCount)
+      overlay.width = overlay.width.clamp(minWidth, maxWidth + 10);
+    else {
+      overlay.width = overlay.width.clamp(
+          minWidth,
+          maxCrossAxisCount * reactGridViewModel.crossAxisStride +
+              crossAxisOverflow +
+              10);
+    }
+
+    overlay.left = startLeft + startWidth - overlay.width;
+
+    overlay.overlayEntry.markNeedsBuild();
+
+    int crossAxisCount =
+        (overlay.width / reactGridViewModel.crossAxisStride).floor();
+    int crossAxisOffsetCount = maxCrossAxisCount - crossAxisCount;
+
+    if (previousCrossAxisCount != crossAxisCount) {
+      previousCrossAxisCount = crossAxisCount;
+      _cubit.resizedChild(
+          widget.index,
+          startModel.copyWith(
+              crossAxisCount: crossAxisCount,
+              crossAxisOffsetCount: crossAxisOffsetCount));
+    }
+  }
+
+  void resizeOnPanUpdateRightCallback(DragUpdateDetails details) {
+    offset += details.delta;
+    overlay.width = startWidth + offset.dx;
+
+    int maxCrossAxisCount =
+        reactGridViewModel.crossAxisCount - startModel.crossAxisOffsetCount;
+    if (maxCrossAxisCount >= startModel.maxCrossAxisCount)
+      overlay.width = overlay.width.clamp(minWidth, maxWidth + 10);
+    else {
+      overlay.width = overlay.width.clamp(
+          minWidth,
+          maxCrossAxisCount * reactGridViewModel.crossAxisStride +
+              crossAxisOverflow +
+              10);
+    }
+
+    overlay.overlayEntry.markNeedsBuild();
+
+    int crossAxisCount =
+        (overlay.width / reactGridViewModel.crossAxisStride).floor();
+
+    if (previousCrossAxisCount != crossAxisCount) {
+      previousCrossAxisCount = crossAxisCount;
+      _cubit.resizedChild(
+          widget.index,
+          startModel.copyWith(
+            crossAxisCount: crossAxisCount,
+          ));
+    }
+  }
+
+  void resizeOnPanUpdateTopCallback(DragUpdateDetails details) {
+    offset += details.delta;
+    overlay.height = startHeight - offset.dy;
+
+    int maxMainAxisCount =
+        startModel.mainAxisOffsetCount + startModel.mainAxisCount;
+    if (maxMainAxisCount >= startModel.maxMainAxisCount)
+      overlay.height = overlay.height.clamp(minHeight, maxHeight + 10);
+    else {
+      overlay.height = overlay.height.clamp(
+          minHeight,
+          maxMainAxisCount * reactGridViewModel.mainAxisStride +
+              mainAxisOverflow +
+              10);
+    }
+
+    overlay.top = startTop + startHeight - overlay.height;
+
+    overlay.overlayEntry.markNeedsBuild();
+
+    int mainAxisCount =
+        (overlay.height / reactGridViewModel.mainAxisStride).floor();
+    int mainAxisOffsetCount = maxMainAxisCount - mainAxisCount;
+
+    if (previousMainAxisCount != mainAxisCount) {
+      previousMainAxisCount = mainAxisCount;
+      _cubit.resizedChild(
+          widget.index,
+          startModel.copyWith(
+              mainAxisCount: mainAxisCount,
+              mainAxisOffsetCount: mainAxisOffsetCount));
+    }
   }
 }
