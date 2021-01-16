262f3b60e6083ffe3bba4c39706205c535632bbb
Hyeonki Hong <hhk7734@gmail.com>
2021-01-17 07:56:57 +0900

    project_list: view: add initState to ProjectListPage

--- lib/project_list/view/project_list_page.dart
+++ lib/project_list/view/project_list_page.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:react_grid_view/react_grid_view.dart';
 
 import '../project_list.dart';
 
-class ProjectListPage extends StatelessWidget {
-  ProjectListPage({
-    Key key,
-    @required ProjectRepository projectRepository,
-  })  : projectRepository = projectRepository,
-        reactGridView = ReactGridView.fromModel(
-          children: projectRepository.projectModelList
-              .map<ReactPositioned>((e) => e.toAvatar())
-              .toList(),
-          model: projectRepository.projectListModel.reactGridViewModel,
-        ),
-        super(key: key);
+class ProjectListPage extends StatefulWidget {
+  @override
+  _ProjectListPageState createState() => _ProjectListPageState();
 
-  final ProjectRepository projectRepository;
-  final ReactGridView reactGridView;
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => ProjectListPage());
+  }
+}
+
+class _ProjectListPageState extends State<ProjectListPage> {
+  ProjectRepository projectRepository;
+  ReactGridView reactGridView;
+
+  @override
+  void initState() {
+    super.initState();
+    projectRepository = context.read<ProjectRepository>();
+    reactGridView = ReactGridView.fromModel(
+      children: projectRepository.projectModelList
+          .map<ReactPositioned>((e) => e.toAvatar())
+          .toList(),
+      model: projectRepository.projectListModel.reactGridViewModel,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,16 +37,5 @@ class ProjectListPage extends StatelessWidget {
       ),
       body: reactGridView,
     );
-  }
-
-  static Route route({
-    Key key,
-    @required ProjectRepository projectRepository,
-  }) {
-    return MaterialPageRoute<void>(
-        builder: (_) => ProjectListPage(
-              key: key,
-              projectRepository: projectRepository,
-            ));
   }
 }
