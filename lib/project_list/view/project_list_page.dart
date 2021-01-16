d49466009908bb29808fb228a2eb3f5f6a977927
Hyeonki Hong <hhk7734@gmail.com>
2021-01-17 08:09:22 +0900

    project_list: view: add create-project-button to ProjectListPage

--- lib/project_list/view/project_list_page.dart
+++ lib/project_list/view/project_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:react_grid_view/react_grid_view.dart';
 
+import '../create_project/create_project.dart';
 import '../project_list.dart';
 import '../../authentication/authentication.dart';
 
@@ -36,6 +37,20 @@ class _ProjectListPageState extends State<ProjectListPage> {
       appBar: AppBar(
         title: Text("Project List"),
         actions: [
+          IconButton(
+            icon: Icon(Icons.add_box),
+            onPressed: () {
+              Navigator.of(context)
+                  .push<bool>(CreateProjectPage.route())
+                  .then((value) {
+                if (value != null && value) {
+                  reactGridView.addChild(
+                      child:
+                          projectRepository.projectModelList.last.toAvatar());
+                }
+              });
+            },
+          ),
           IconButton(
             icon: Icon(Icons.logout),
             onPressed: () {
